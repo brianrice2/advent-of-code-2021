4625f661b435eb1e0e54ff7eecbf739db5b56241
Brian Rice <brianrice013@gmail.com>
2021-12-01 19:35:25 -0600

    Day 1

---
+++ day01.py
@@ -0,0 +1,21 @@
+with open("input1.txt", "r") as file:
+    nums = list(map(int, file.read().splitlines()))
+
+
+# Part 1
+count = 0
+for i in range(1, len(nums)):
+    if nums[i] > nums[i-1]:
+        count += 1
+print(count)
+
+
+# Part 2
+count = 0
+sliding_sums = []
+for start_idx in range(0, len(nums)-2):
+    sliding_sum = sum(nums[start_idx:start_idx+3])
+    sliding_sums.append(sliding_sum)
+    if start_idx > 0 and sliding_sums[start_idx] > sliding_sums[start_idx - 1]:
+        count += 1
+print(count)
